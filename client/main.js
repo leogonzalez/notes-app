51f3a0b941463a47c89a22235fcec70b3fa874a2
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2017-06-29 07:32:46 -0300

    Setup mobile navigation

--- client/main.js
+++ client/main.js
@@ -14,12 +14,18 @@ Tracker.autorun(() => {
 
 Tracker.autorun(() => {
   const selectedNoteId = Session.get('selectedNoteId');
+  Session.set('isNavOpen',false);
   if (selectedNoteId) {
     // used brwoser history
     browserHistory.replace(`/dashboard/${selectedNoteId}`);
+
   }
   // new route created!
+});
 
+Tracker.autorun(() => {
+  const isNavOpen = Session.get('isNavOpen');
+  document.body.classList.toggle('is-nav-open',isNavOpen);
 });
 //call tracker
 
@@ -27,5 +33,6 @@ Tracker.autorun(() => {
 // Are Sessions always started in client main.js????
 Meteor.startup(() => {
   Session.set('selectedNoteId',undefined);
+  Session.set('isNavOpen',false);
   ReactDOM.render(routes, document.getElementById('app'));
 });
